0816d1daa234cff32b6f5f9f36081fd25df669a5
Saurabh Thakur <shaurabhnarayan@gmail.com>
2025-08-24 00:36:28 +0530

    final touchups

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// Initialize EmailJS with your Public Key
+emailjs.init("vhZEsKjwEpChCsy2t");  
+
+messagesent = document.getElementById("messagesent")
+// Handle form submission
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const serviceID = "service_1gydj9p";   // from EmailJS dashboard
+  const templateID = "template_8f3iwyw"; // from EmailJS dashboard
+
+  emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      messagesent.textContent = "Message sent Successfully!";
+      this.reset(); // clear form
+    })
+    .catch((error) => {
+      console.error("Failed to send:", error);
+      alert("Failed to send message. Please try again.");
+    });
+});
+
+
+
 // Initialize EmailJS
 emailjs.init("vhZEsKjwEpChCsy2t");  
 
