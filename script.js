544d39e8922beb6cea69a2277424f7c2edb184a6
Saurabh Thakur <shaurabhnarayan@gmail.com>
2025-08-23 17:23:37 +0530

    Initial commit - clean static site

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-function sendMail(){
+function sendMail() {
     let parms = {
         name : document.getElementById("name").value,
         email : document.getElementById("email").value,
         message : document.getElementById("message").value
+    };
 
-    }
-
-    emailjs.send("service_1gydj9p","template_8f3iwyw",parms).then(alert("Email Sent!"))
+    emailjs.send("service_1gydj9p", "template_8f3iwyw", parms, "vhZEsKjwEpChCsy2t")
+    .then(() => {
+        alert("Email Sent!");
+    })
+    .catch((err) => {
+        console.log("FAILED...", err);
+        alert("Failed to send email. Try again later.");
+    });
 }
